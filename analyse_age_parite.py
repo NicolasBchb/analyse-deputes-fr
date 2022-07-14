087737269fee86198141f437d6474c0815861bcc
firstlink <nicolas.bchb@outlook.fr>
2022-07-14 22:30:53 +0200

    MaJ Stream du 14/07 - Ajout Cartos Hashtags Interactives

--- analyse_age_parite.py
+++ analyse_age_parite.py
@@ -1,13 +1,9 @@
 # %% import modules
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
 import unidecode
-from tqdm import tqdm
-import ast
-import os
 import json
-import dtale
+from math import log10
 
 # %% load data
 df_deputes = {
@@ -19,17 +15,17 @@ df_deputes = {
 # %% prepare data
 partis_merger = {
     "2022": {
-        "Écologiste - Nupes":"NUPES",
-        "Non inscrit":"Autres",
-        "Rassemblement National":"RN",
-        "Renaissance":"Ensemble",
-        "La France insoumise - Nouvelle Union Populaire écologique et sociale":"NUPES",
-        "Horizons et apparentés":"Ensemble",
-        "Socialistes et apparentés (membre de l’intergroupe Nupes)":"NUPES",
-        "Gauche démocrate et républicaine - Nupes":"NUPES",
-        "Libertés, Indépendants, Outre-mer et Territoires":"Autres",
-        "Les Républicains":"LR",
-        "Démocrate (MoDem et Indépendants)":"Ensemble",
+        "Écologiste - Nupes": "NUPES",
+        "Non inscrit": "Autres",
+        "Rassemblement National": "RN",
+        "Renaissance": "Ensemble",
+        "La France insoumise - Nouvelle Union Populaire écologique et sociale": "NUPES",
+        "Horizons et apparentés": "Ensemble",
+        "Socialistes et apparentés (membre de l’intergroupe Nupes)": "NUPES",
+        "Gauche démocrate et républicaine - Nupes": "NUPES",
+        "Libertés, Indépendants, Outre-mer et Territoires": "Autres",
+        "Les Républicains": "LR",
+        "Démocrate (MoDem et Indépendants)": "Ensemble",
     },
     "2017": {
         "Écologie Démocratie Solidarité": "Autres",
@@ -67,7 +63,7 @@ partis_colors = {
         "Autres": "grey",
         "RN": "SaddleBrown",
         "Ensemble": "orange",
-        "LR": "blue"
+        "LR": "blue",
     },
     "2017": {
         "Autres": "grey",
@@ -75,32 +71,41 @@ partis_colors = {
         "LREM": "orange",
         "LFI": "red",
         "PS": "HotPink",
-        "LR": "blue"
+        "LR": "blue",
     },
-    "2012": {
-        "Autres": "grey",
-        "UMP": "blue",
-        "PS": "HotPink",
-        "UDI": "turquoise"
-    }
+    "2012": {"Autres": "grey", "UMP": "blue", "PS": "HotPink", "UDI": "turquoise"},
 }
 
 for mandat in df_deputes:
     df_deputes[mandat]["etatCivil.infoNaissance.dateNais"] = pd.to_datetime(
         df_deputes[mandat]["etatCivil.infoNaissance.dateNais"]
     )
-    df_deputes[mandat]["age"] = df_deputes[mandat]["etatCivil.infoNaissance.dateNais"].apply(
-        lambda x: (int(mandat) - x.year)
-    )
+    df_deputes[mandat]["age"] = df_deputes[mandat][
+        "etatCivil.infoNaissance.dateNais"
+    ].apply(lambda x: (int(mandat) - x.year))
 
     df_deputes[mandat]["count"] = 1
     df_deputes[mandat]["sexe"] = 1
-    df_deputes[mandat]["sexe"][df_deputes[mandat]["etatCivil.ident.civ"] == "Mme"] = "Femmes"
-    df_deputes[mandat]["sexe"][df_deputes[mandat]["etatCivil.ident.civ"] == "M."] = "Hommes"
-        
-    df_deputes[mandat]["parti"] = df_deputes[mandat]["groupe"].map(partis_merger[mandat])
+    df_deputes[mandat]["sexe"][
+        df_deputes[mandat]["etatCivil.ident.civ"] == "Mme"
+    ] = "Femmes"
+    df_deputes[mandat]["sexe"][
+        df_deputes[mandat]["etatCivil.ident.civ"] == "M."
+    ] = "Hommes"
 
-    df_deputes[mandat]["nom"] = df_deputes[mandat]["etatCivil.ident.nom"].map(lambda x: unidecode.unidecode(x).capitalize()) + " " + df_deputes[mandat]["etatCivil.ident.prenom"].map(lambda x: unidecode.unidecode(x).capitalize())
+    df_deputes[mandat]["parti"] = df_deputes[mandat]["groupe"].map(
+        partis_merger[mandat]
+    )
+
+    df_deputes[mandat]["nom"] = (
+        df_deputes[mandat]["etatCivil.ident.nom"].map(
+            lambda x: unidecode.unidecode(x).capitalize()
+        )
+        + " "
+        + df_deputes[mandat]["etatCivil.ident.prenom"].map(
+            lambda x: unidecode.unidecode(x).capitalize()
+        )
+    )
 
 
 # %% plot boxplot of age by party for each mandat (2012, 2017, 2022)
@@ -129,7 +134,6 @@ for mandat in df_deputes:
     ages_box.write_html(f"viz/ages_{mandat}.html")
 
 
-
 # %% plot sunburst of parite by party for each mandat (2012, 2017, 2022)
 for mandat in df_deputes:
     parite = px.sunburst(
@@ -155,19 +159,19 @@ for mandat in df_deputes:
         x="age",
         y="count",
         nbins=df_deputes[mandat]["age"].nunique(),
-        marginal="box", # can be `rug`, `violin`
+        marginal="box",  # can be `rug`, `violin`
     )
     ages.update_layout(
         title_text=f"<b>Histogramme des âges des députés ({mandat})</b>",
         margin=dict(l=0, r=0, t=70, b=40),
         plot_bgcolor="rgb(243, 243, 243)",
-        bargap=0.2
+        bargap=0.2,
     )
     ages.update_xaxes(title_text="<b><i>Âge</i></b>")
     ages.update_yaxes(title_text="<b>Nombre de députés</b>")
     ages.write_html(f"viz/ages_histo_{mandat}.html")
 
-    
+
 # %%
 for mandat in df_deputes:
     df_deputes[mandat]["annee_mandat"] = mandat
@@ -179,9 +183,9 @@ ages = px.histogram(
     x="age",
     y="count",
     nbins=df_deputes[mandat]["age"].nunique(),
-    marginal="box", # can be `rug`, `violin`,
+    marginal="box",  # can be `rug`, `violin`,
     color="annee_mandat",
-    barmode='group',
+    barmode="group",
     color_discrete_sequence=px.colors.qualitative.Safe,
     color_discrete_map={
         "2012": "#F0244D",
@@ -196,7 +200,7 @@ ages.update_layout(
     title_text="<b>Répartition des âges des députés par mandat</b>",
     margin=dict(l=0, r=0, t=70, b=40),
     plot_bgcolor="rgb(243, 243, 243)",
-    bargap=0.3
+    bargap=0.3,
 )
 ages.update_xaxes(title_text="<b><i>Âge</i></b>")
 ages.update_yaxes(title_text="<b>Nombre de députés</b>")
@@ -205,6 +209,202 @@ ages.write_html("viz/ages_histo_global.html")
 
 # %% professions (pas 2012)
 
+for mandat in ["2017", "2022"]:
 
+    fig = px.bar(
+        df_deputes[mandat]
+        .groupby(
+            ["profession.socProcINSEE.catSocPro", "profession.socProcINSEE.famSocPro"]
+        )
+        .sum()
+        .reset_index(),
+        x="profession.socProcINSEE.famSocPro",
+        y="count",
+        color="profession.socProcINSEE.catSocPro",
+        text="profession.socProcINSEE.catSocPro",
+        log_y=True,
+    )
+    fig.update_layout(
+        title_text=f"<b>Répartition des professions des députés ({mandat})</b>",
+        margin=dict(l=0, r=0, t=70, b=40),
+        plot_bgcolor="rgb(243, 243, 243)",
+    )
+    fig.update_xaxes(
+        title_text="<b><i>Profession</i></b>", categoryorder="total descending"
+    )
+    fig.update_yaxes(title_text="<b>Nombre de députés</b>")
+
+    fig.write_html(f"viz/professions_{mandat}.html")
+
+
+# %% professions sun (pas 2012)
+
+for mandat in ["2017", "2022"]:
+
+    fig = px.sunburst(
+        df_deputes[mandat]
+        .groupby(
+            ["profession.socProcINSEE.catSocPro", "profession.socProcINSEE.famSocPro"]
+        )
+        .sum()
+        .reset_index()
+        .sort_values(by="count", ascending=False),
+        path=["profession.socProcINSEE.famSocPro", "profession.socProcINSEE.catSocPro"],
+        values="count",
+        color="profession.socProcINSEE.famSocPro",
+        color_discrete_sequence=px.colors.qualitative.T10,
+    )
+    fig.update_layout(
+        title_text=f"<b>Répartition des professions des députés ({mandat})</b>",
+        margin=dict(l=0, r=0, t=70, b=40),
+        plot_bgcolor="rgb(243, 243, 243)",
+    )
+    fig.update_traces(textinfo="label+percent parent", textfont=dict(size=16))
+    fig.write_html(f"viz/professions_sun_{mandat}.html")
+
+# %% professions (pas 2012)
+
+for mandat in ["2017", "2022"]:
+
+    fig = px.bar(
+        df_deputes[mandat]
+        .groupby(["profession.socProcINSEE.famSocPro"])
+        .sum()
+        .reset_index()
+        .sort_values(by="count", ascending=False),
+        x="profession.socProcINSEE.famSocPro",
+        y="count",
+        color="profession.socProcINSEE.famSocPro",
+        color_discrete_sequence=px.colors.qualitative.Safe
+        # log_y=True,
+    )
+    fig.update_layout(
+        title_text=f"<b>Répartition des professions des députés ({mandat})</b>",
+        margin=dict(l=0, r=0, t=70, b=40),
+        plot_bgcolor="rgb(243, 243, 243)",
+    )
+    fig.update_xaxes(
+        title_text="<b><i>Profession</i></b>", categoryorder="total descending"
+    )
+    fig.update_yaxes(title_text="<b>Nombre de députés</b>")
+    fig.update_layout(showlegend=False)
+    fig.write_html(f"viz/professions_bar_{mandat}.html")
 
 # %% lieux de naissance
+with open("carte\departements.json") as response:
+    dep = json.load(response)
+
+for mandat in df_deputes:
+
+    df_temp = df_deputes[mandat].copy()
+    df_temp = df_temp.groupby("etatCivil.infoNaissance.depNais").sum().reset_index()
+    df_temp["logcount"] = df_temp["count"].map(lambda x: log10(x) * 100)
+
+    dep_map = px.choropleth_mapbox(
+        df_temp,
+        geojson=dep,
+        locations="etatCivil.infoNaissance.depNais",
+        color="logcount",
+        color_continuous_scale="OrRd",
+        hover_data=["etatCivil.infoNaissance.depNais", "count"],
+        range_color=(0, 160),
+        mapbox_style="carto-positron",
+        zoom=6,
+        center={"lat": 47.081012, "lon": 2.398782},
+        opacity=0.75,
+        featureidkey="properties.nom",
+    )
+
+    dep_map.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        title_text=f"<b>Répartition des députés par département ({mandat})</b>",
+        showlegend=False,
+    )
+
+    dep_map.write_html(f"viz/departements_map_{mandat}.html")
+
+    fig_bar = px.bar(
+        df_temp,
+        x="etatCivil.infoNaissance.depNais",
+        y="count",
+        color="logcount",
+        color_continuous_scale="OrRd",
+        hover_data=["etatCivil.infoNaissance.depNais", "count"],
+        range_color=(0, 160),
+    )
+
+    fig_bar.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        title_text=f"<b>Répartition des députés par département ({mandat})</b>",
+        showlegend=False,
+    )
+
+    fig_bar.update_xaxes(
+        title_text="<b><i>Département</i></b>", categoryorder="total descending"
+    )
+
+    fig_bar.update_yaxes(title_text="<b>Nombre de députés</b>")
+
+    fig_bar.write_html(f"viz/departements_bar_{mandat}.html")
+
+# %% ratio de députés en fonction de la population
+pop_dep = pd.read_csv("data/Departements.csv", sep=";")
+pop_dep_dict = dict(zip(pop_dep["DEP"], pop_dep["PTOT"]))
+
+for mandat in df_deputes:
+
+    df_temp = df_deputes[mandat].copy()
+    df_temp = df_temp.groupby("etatCivil.infoNaissance.depNais").sum().reset_index()
+    df_temp["logcount"] = df_temp["count"].map(lambda x: log10(x) * 100)
+    df_temp["pop"] = df_temp["etatCivil.infoNaissance.depNais"].map(pop_dep_dict)
+    df_temp["ratio"] = df_temp["pop"] / df_temp["count"]
+
+    dep_map = px.choropleth_mapbox(
+        df_temp,
+        geojson=dep,
+        locations="etatCivil.infoNaissance.depNais",
+        color="ratio",
+        color_continuous_scale="OrRd_r",
+        hover_data=["etatCivil.infoNaissance.depNais", "ratio"],
+        # range_color=(0, 160),
+        mapbox_style="carto-positron",
+        zoom=6,
+        center={"lat": 47.081012, "lon": 2.398782},
+        opacity=0.75,
+        featureidkey="properties.nom",
+    )
+
+    dep_map.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        title_text=f"<b>Répartition des députés par département ({mandat})</b>",
+        showlegend=False,
+    )
+
+    dep_map.write_html(f"viz/ratio_map_{mandat}.html")
+
+    fig_bar = px.bar(
+        df_temp,
+        x="etatCivil.infoNaissance.depNais",
+        y="ratio",
+        color="ratio",
+        color_continuous_scale="OrRd_r",
+        hover_data=["etatCivil.infoNaissance.depNais", "ratio"],
+        # range_color=(0, 160)
+    )
+
+    fig_bar.update_layout(
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        title_text=f"<b>Répartition des députés par département ({mandat})</b>",
+        showlegend=False,
+    )
+
+    fig_bar.update_xaxes(
+        title_text="<b><i>Département</i></b>", categoryorder="total ascending"
+    )
+
+    fig_bar.update_yaxes(title_text="<b>Nombre de députés</b>")
+
+    fig_bar.write_html(f"viz/ratio_bar_{mandat}.html")
+
+
+# %%
--- analyse_tweets.py
+++ analyse_tweets.py
@@ -13,59 +13,64 @@ from collections import Counter
 
 # %% load data
 df_deputes = {
-    mandat: 
-    pd.read_csv(f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_clean.csv")
+    mandat: pd.read_csv(
+        f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_clean.csv"
+    )
     for mandat in ["2012", "2017", "2022"]
-    }
+}
 
 
 # %%
-def tokenize(text):
-    """
-    Tokenize text
-    """
-    tokens = nltk.word_tokenize(text)
-    # remove punctuation
-    tokens = [token.lower() for token in tokens if token.isalpha()]
-    return [token.lower() for token in tokens]
+for mandat in df_deputes:
+    df = df_deputes[mandat].copy()
+    df["date"] = pd.to_datetime(df["date"])
+    df["date"] = df["date"].dt.date
+    df["hashtags"] = df["hashtags"].map(ast.literal_eval)
+    df = df.explode("hashtags")
 
-def remove_stopwords(token):
-    """
-    Remove stopwords from text
-    """
-    stop_words = stopwords.words("french")
-    stop_words += ["http", "https", "a"]
-    return " ".join([word for word in token if word not in stop_words])
+    hashtags_list = list(
+        df.dropna(axis=0, subset="hashtags").query("hashtags != '[]'")["hashtags"]
+    )
 
-def clean_text(text):
-    """
-    Clean text
-    """
-    tokens = tokenize(text)
-    tokens = remove_stopwords(tokens)
-    text = unidecode.unidecode(tokens)
-    return text
+    df_timestamps = pd.DataFrame([], columns=["hashtags", "date", "type"])
+    for hashtag in tqdm(set(hashtags_list)):
+        df_hash = df.query(f"hashtags == '{hashtag}'")
+        df_first = df_hash.sort_values("date").head(1)
+        df_first["type"] = "first"
+        df_last = df_hash.sort_values("date").tail(1)
+        df_last["type"] = "last"
+        df_timestamps = pd.concat(
+            [
+                df_timestamps,
+                df_first[["hashtags", "date", "type"]],
+                df_last[["hashtags", "date", "type"]],
+            ]
+        )
 
-def count_frequency(text):
-    """
-    Count frequency of words in text
-    """
-    tokens = tokenize(text)
-    return nltk.FreqDist(tokens)
-
-# %%
-for mandat in tqdm(df_deputes):
-    df_deputes[mandat]["tweet_clean"] = df_deputes[mandat]["tweet"].map(clean_text)
-    df_deputes[mandat]["tweet_frequency"] = df_deputes[mandat]["tweet_clean"].map(count_frequency)
-
-dtale.show(df_deputes["2012"])
+    df_timestamps.to_csv(
+        f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_timestamps.csv",
+        index=False,
+    )
+    # dtale.show(df_timestamps).open_browser()
 
 
 # %%
-freq = {}
-for mandat in tqdm(df_deputes):
-    df_deputes[mandat].groupby(['parti'])['tweet_clean'].apply(','.join).reset_index()
-    for parti in df_deputes[mandat]['parti']:
-        # save value in variable
-        text = df_deputes[mandat]['tweet_clean'][df_deputes[mandat]['parti'] == parti]
-        freq[mandat] = Counter(text.split()).most_common()
+for mandat in df_deputes:
+    df_timestamps = pd.read_csv(f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_timestamps.csv")
+    df_timestamps["hashtags"] = "#" + df_timestamps["hashtags"]
+    df_start = df_timestamps.query("type == 'first'")
+    df_end = df_timestamps.query("type == 'last'")
+    
+    start = dict(zip(df_start["hashtags"], df_start["date"]))
+    end = dict(zip(df_end["hashtags"], df_end["date"]))
+
+    df_nodes = pd.read_csv(f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_hashtags_nodes.csv")
+    df_nodes["start"] = df_nodes["label"].map(start)
+    df_nodes["end"] = df_nodes["label"].map(end)
+    df_nodes.to_csv(
+        f"twitter_data/deputes_{mandat}_Twitter/deputes_{mandat}_Twitter_hashtags_nodes_timestamps.csv",
+        index=False,
+        )
+
+        
+# %%
